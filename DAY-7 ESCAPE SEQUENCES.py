9f553fd485c4b6c33cdfd08ec09cec307c4338dd
kavurivenkateshreddy <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-04-30 16:51:35 +0530

    Add files via upload

---
+++ DAY-7 ESCAPE SEQUENCES.py
@@ -0,0 +1,56 @@
+"""
+Date: 27th April 2022 wednesday
+Name: kavuri venkatesh reddy
+File Desc: print statement
+"""
+print('hi i am hear to \
+configure a test framework \
+to see your tests here') #hi i am hear to configure a test framework to see your tests here
+print("hi i am hear to \
+configure a test framework \
+to see your tests here")   #hi i am hear to configure a test framework to see your tests here
+print(len('hi i am hear to configure a test framework to see your tests here')) #65
+print('escape\'sequences')        #escape'sequences
+print('escape"sequences')         #escape"sequences
+print("escape\"sequences")        #escape"sequences
+print("escape'sequences")         #escape'sequences
+print('''escape'sequ\'''ences''') #escape'sequ'''ences
+print("""escape's"equ\"""ences""") #escape's"equ"""ences
+
+print('escape' 'sequences')       #escapesequences       
+print('escape\n' 'sequences')     #escape
+                                  #sequences
+print('escape' '\nsequences')     #escape
+                                  #sequences
+print('escape','sequences')       #escape sequences
+print('escape','sequences',sep='\n') #escape
+                                     #sequences
+print('escape'+'\n'+'sequences')     #escape
+                                     #sequences
+
+print('C:\\Users\Venkatesh\KVR\python') #C:\Users\Venkatesh\KVR\python
+
+# print('F:\Uatch_9_15_AM')                # Error
+
+print('C:\\Users\\venkatesh')             #C:\Users\venkatesh
+
+print("C:\\Users\\venkatesh\\Downloads")  #C:\Users\venkatesh\Downloads
+
+print('\u0001')     #☺
+print('\u0002')     #☻
+print('\u0003')     #♥
+print('\u0004')     #♦
+print('\u0005')     #♣
+print('\u0006')     #♠
+print('\u0007')     #
+print('\u0008')     #
+print('\u0009')     #
+print('\U0001f601')   #😁
+print('\U0001f602')   #😂
+print('\U0001f603')   #😃
+print('\U0001f604')   #😄
+print('\U0001f605')   #😅
+
+print("C:\\Users\\venkatesh\\Desktop\\915Am")      #C:\Users\venkatesh\Desktop\915Am
+
+
