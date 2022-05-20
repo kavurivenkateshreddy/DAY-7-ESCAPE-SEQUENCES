aee5c0d9c6242fdac7c811f148c8043bf3d72051
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-05-20 19:48:03 +0530

    Update DAY-7 ESCAPE SEQUENCES.py

--- DAY-7 ESCAPE SEQUENCES.py
+++ DAY-7 ESCAPE SEQUENCES.py
@@ -3,54 +3,108 @@ Date: 27th April 2022 wednesday
 Name: kavuri venkatesh reddy
 File Desc: print statement
 """
-print('hi i am hear to \
-configure a test framework \
-to see your tests here') #hi i am hear to configure a test framework to see your tests here
-print("hi i am hear to \
-configure a test framework \
-to see your tests here")   #hi i am hear to configure a test framework to see your tests here
-print(len('hi i am hear to configure a test framework to see your tests here')) #65
-print('escape\'sequences')        #escape'sequences
-print('escape"sequences')         #escape"sequences
-print("escape\"sequences")        #escape"sequences
-print("escape'sequences")         #escape'sequences
-print('''escape'sequ\'''ences''') #escape'sequ'''ences
-print("""escape's"equ\"""ences""") #escape's"equ"""ences
+# print('bye\akhil')   # byekhil       --->\a alert
+# print('bye\aakhil')  # byeakhil
+# print('bye\\akhil')  # bye\akhil
 
-print('escape' 'sequences')       #escapesequences       
-print('escape\n' 'sequences')     #escape
-                                  #sequences
-print('escape' '\nsequences')     #escape
-                                  #sequences
-print('escape','sequences')       #escape sequences
-print('escape','sequences',sep='\n') #escape
-                                     #sequences
-print('escape'+'\n'+'sequences')     #escape
-                                     #sequences
+# print('hi\bharath')  # hharath       --->\b backspace
+# print('hi \bharath') # hiharath
+# print('hi\\bharath') # hi\bharath
 
-print('C:\\Users\Venkatesh\KVR\python') #C:\Users\Venkatesh\KVR\python
+# print('hi\charan')   #hi\charan      --->\c no meaning
+# print('hi\david')    #hi\david       --->\d no meaning
+# print('hi\eshwar')   #hi\eshwar      --->\e no meaning
 
-# print('F:\Uatch_9_15_AM')                # Error
+# print('hi\fasi')     #hi             --->\f new linr or female symble
+                       #asi
 
-print('C:\\Users\\venkatesh')             #C:\Users\venkatesh
+# print('hi\ganesh')   #hi\ganesh      --->\g no meaning
+# print('hi\harish')   #hi\harish      --->\h no meaning
+# print('hi\ishan')    #hi\ishan       --->\i no meaning
+# print('hi\jai')      #hi\jai         --->\j no meaning
+# print('hi\karthik')  #hi\karthik     --->\k no meaning
+# print('hi\lakshman') #hi\lakshman    --->\l no meaning
+# print('hi\manish')   #hi\manish      --->\m no meaning
 
-print("C:\\Users\\venkatesh\\Downloads")  #C:\Users\venkatesh\Downloads
+# print('hi\navya')    #hi             --->\n new line
+                       #avya
 
-print('\u0001')     #☺
-print('\u0002')     #☻
-print('\u0003')     #♥
-print('\u0004')     #♦
-print('\u0005')     #♣
-print('\u0006')     #♠
-print('\u0007')     #
-print('\u0008')     #
-print('\u0009')     #
-print('\U0001f601')   #😁
-print('\U0001f602')   #😂
-print('\U0001f603')   #😃
-print('\U0001f604')   #😄
-print('\U0001f605')   #😅
+# print('hi\omprakash') #hi\omprakash  --->\o no meaning
+# print('hi\pankaj')    #hi\pankaj     --->\p no meaning
+# print('hi\queen')     #hi\queen      --->\q no meaning
 
-print("C:\\Users\\venkatesh\\Desktop\\915Am")      #C:\Users\venkatesh\Desktop\915Am
+# print('hi\ramesh')# amesh --->right side values of \r wii replace the values of left side starting letters
+# print('hi\ram') # am                 --->\r carriage return
 
+# print('hello\ram') # amllo
 
+# print('hello\ramesh') # amesh
+# print('hello\rames')  # ameso
+# print('heyhi\ramya ') # amya 
+
+# print('hi\system')    #hi\system     --->\s no meaning
+
+#print('hello\ten')     # \t is negleted because the spaces are not defined correctly
+# print('hello\\ten')   #hello\ten     --->\t tab
+
+# print('hi\umesh')     #hi\umesh      --->\u unocode
+
+# print('\u0001')
+# print('\u0002')
+# print('\u0003')
+# print('\u0004')
+# print('\u0005')
+# print('\u0006')
+# print('\u0007')
+# print('\u0008')
+# print('\u0009')
+
+# print('hi\vriddi')  #hi         --->\v new line
+                      #riddi
+
+# print('hi\worng')   #hi\worng  --->\w no meaning 
+
+# print('hi\xavier')  #unicode error  --->\x unicode
+
+# print('hi\yash')    #hi\yash   --->\y no meaning
+# print('hi\zayn')    #hi\zayan  --->\z no meaning
+
+# print('hi\Naveen')  # captal U AND N are unicodes
+# print('hi\ Usha')
+
+# print('hi i am hear to \          --->\ continution
+# configure a test framework \
+# to see your tests here')          #hi i am hear to configure a test framework to see your tests here
+
+# print("hi i am hear to \          --->\ continution
+# configure a test framework \
+# to see your tests here")          #hi i am hear to configure a test framework to see your tests here
+
+# print(len('hi i am hear to configure a test framework to see your tests here')) #65
+
+# print('escape\'sequences')           #escape'sequences  --->\' single quodes
+# print('escape"sequences')            #escape"sequences
+
+# print("escape\"sequences")           #escape"sequences  --->\" double quotes
+# print("escape'sequences")            #escape'sequences
+# print('''escape'sequ\'''ences''')    #escape'sequ'''ences
+# print("""escape's"equ\"""ences""")   #escape's"equ"""ences
+
+# print('escape' 'sequences')          #escapesequences       
+# print('escape\n' 'sequences')        #escape
+#                                      #sequences
+# print('escape' '\nsequences')        #escape
+#                                      #sequences
+# print('escape','sequences')          #escape sequences
+# print('escape','sequences',sep='\n') #escape
+#                                      #sequences
+# print('escape'+'\n'+'sequences')     #escape
+#                                      #sequences
+
+# print('C:\\Users\Venkatesh\KVR\python')   #C:\Users\Venkatesh\KVR\python
+
+# # print('F:\Uatch_9_15_AM')               # Error
+
+# print('C:\\Users\\venkatesh')             #C:\Users\venkatesh --->\\ it gives single quotes
+
+# print("C:\\Users\\venkatesh\\Downloads")  #C:\Users\venkatesh\Downloads
